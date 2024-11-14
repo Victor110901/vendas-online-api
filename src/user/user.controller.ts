76e174ff83e14dc49def3fd812caa7b36022ef94
Victor.Santos <victor.santos@pdcase.com.br>
2024-11-14 17:11:21 -0300

    0002: Adicionando validação e padronizando retorno de usuarios

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,20 +1,24 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, UsePipes, ValidationPipe } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create.user.dto';
 import { UserService } from './user.service';
 import { UserEntity } from './entity/user.entity';
+import { ReturnUserDto } from './dtos/return.user.dto';
 
 @Controller('user')
 export class UserController {
 
     constructor(private readonly userService: UserService) {}
 
+    @UsePipes(ValidationPipe)
     @Post()
     async createUser(@Body() createUser: CreateUserDto): Promise<UserEntity> {
         return this.userService.createUser(createUser);
     }
 
     @Get()
-    async getAllUsers(): Promise<UserEntity[]> {
-        return this.userService.getAllUser();
+    async getAllUsers(): Promise<ReturnUserDto[]> {
+        return (await this.userService.getAllUser()).map(
+            (userEntity) => new ReturnUserDto(userEntity),
+        );
     }
 }
