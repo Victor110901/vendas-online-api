55cd43f6b352db6fa1a1fa76fa0a13f3802d41d4
Victor.Santos <victor.santos@pdcase.com.br>
2024-11-14 17:50:51 -0300

    0002: Validação de erros ao buscar usuario e cidade inexistentes

--- src/address/address.module.ts
+++ src/address/address.module.ts
@@ -3,9 +3,11 @@ import { AddressController } from './address.controller';
 import { AddressService } from './address.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AddressEntity } from './entity/address.entity';
+import { UserModule } from 'src/user/user.module';
+import { CityService } from 'src/city/city.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([AddressEntity])],
+  imports: [TypeOrmModule.forFeature([AddressEntity]), UserModule, CityService],
   controllers: [AddressController],
   providers: [AddressService]
 })
