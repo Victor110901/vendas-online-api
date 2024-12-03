d3a7db479e351d41a8458f8ed457f895b9bce469
Victor110901 <victor.cs2001.vc@gmail.com>
2024-12-02 21:49:22 -0300

    0005: Adicionando permição dos controllers

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -5,6 +5,8 @@ import { UserEntity } from './entity/user.entity';
 import { ReturnUserDto } from './dtos/return.user.dto';
 import { UpdatePasswordDTO } from './dtos/update-password.dto';
 import { UserId } from '../decorators/user-id.decorator';
+import { Roles } from 'src/decorators/roles.decorator';
+import { UserType } from './enum/user-type.enum';
 
 @Controller('user')
 export class UserController {
@@ -17,6 +19,7 @@ export class UserController {
         return this.userService.createUser(createUser);
     }
 
+    @Roles(UserType.Admin)
     @Get()
     async getAllUsers(): Promise<ReturnUserDto[]> {
         return (await this.userService.getAllUser()).map(
@@ -24,6 +27,7 @@ export class UserController {
         );
     }
 
+    @Roles(UserType.Admin) 
     @Get('/:userId')
     async getUserById(@Param('userId') userId: number): Promise<ReturnUserDto> {
         return new ReturnUserDto(
@@ -31,6 +35,7 @@ export class UserController {
         )
     }
 
+    @Roles(UserType.Admin, UserType.User)
     @Patch()
     @UsePipes(ValidationPipe)
     async updatePasswordUser(
