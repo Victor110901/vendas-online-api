f99b2eff35d0a41a3757f6a32a876faa16b9304d
Victor110901 <victor.cs2001.vc@gmail.com>
2024-12-09 21:44:38 -0300

    0005: verificando se produto existe e tirando itens desnecessarios do retorno

--- src/cart/cart.controller.ts
+++ src/cart/cart.controller.ts
@@ -5,6 +5,7 @@ import { CartEntity } from './entity/cart.entity';
 import { InsertCartDto } from './dtos/insert-cart.dto';
 import { CartService } from './cart.service';
 import { UserId } from 'src/decorators/user-id.decorator';
+import { ReturnCartDto } from './dtos/return-cart.dto';
 
 @Roles(UserType.User, UserType.Admin)
 @Controller('cart')
@@ -18,7 +19,9 @@ export class CartController {
     async insertProductInCart(
         @Body() insertCart: InsertCartDto,
         @UserId() userId: number
-    ): Promise<CartEntity> {
-        return this.cartService.insertProductInCart(insertCart, userId);
+    ): Promise<ReturnCartDto> {
+        return new ReturnCartDto(
+            await this.cartService.insertProductInCart(insertCart, userId)
+        );
     }
 }
